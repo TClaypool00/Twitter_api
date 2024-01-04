d4df3c31ea29db608e4538a8735cbc051c733189
claypooltyler00 <claypooltyler00@gmail.com>
2024-01-04 00:16:30 -0600

    Database logic is less redundant

--- database/database.js
+++ database/database.js
@@ -1,21 +1,19 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: process.env.DEV_DB_HOST,
-    user: process.env.DEV_DB_USER_NAME,
-    password: process.env.DEV_DB_PASSWORD,
-    database: process.env.DEV_DATABASE
-});
-
-
-
 module.exports.connect = function connect() {
+    let connection = mysql.createConnection({
+        host: process.env.DEV_DB_HOST,
+        user: process.env.DEV_DB_USER_NAME,
+        password: process.env.DEV_DB_PASSWORD,
+        database: process.env.DEV_DATABASE
+    });    
+
     connection.connect((err) => {
         if (err) {
             throw err;
         }
     });
-}
 
-module.exports.connection = connection;
+    return connection;
+}
