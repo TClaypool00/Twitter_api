b0e4986de1f75a0c1bf014768d88b01e1040bf8a
claypooltyler00 <claypooltyler00@gmail.com>
2024-01-17 09:00:17 -0600

    Now hashes passwords

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,31 @@ const router = express.Router();
 const User = require('../models/User.js');
 const userService = require('../data_access/userService.js');
 const functions = require('../helpers/globalFunctions.js');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 router.post('/', (req, resp) => {
-
-    console.log(req.body);
     let user = new User(req.body);
-    try{
-        userService.insertUser(user);
 
-        resp.send(functions.getStatus('user has been created'));
+    try{
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) {
+                throw err;
+            }
+
+            bcrypt.hash(user.passowrd, salt, (err, hash) => {
+
+                if (err) {
+                    throw err;
+                }
+
+                user.passowrd = hash;
+
+                userService.insertUser(user);
+
+                resp.send(functions.getStatus('user has been created'));
+            });
+        });
     } catch (ex) {
         functions.getStatus(ex);
     }
