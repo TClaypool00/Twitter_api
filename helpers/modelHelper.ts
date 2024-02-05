a2793082c01713fc43b46159d732b876eb41eac1
claypooltyler00 <claypooltyler00@gmail.com>
2024-02-04 22:36:16 -0600

    Added methods to return objects for Tweet and User; Made methods cleaner/less code

--- helpers/modelHelper.ts
+++ helpers/modelHelper.ts
@@ -1,15 +1,37 @@
+import Tweet from "../models/Tweet";
+import { tweetValuesObject } from "./valuesHelper";
+
 export function requiredIsNull(value : string | undefined | null) : boolean {
-    if (value === null || value === undefined || value === '') {
-        return true;
-    } else {
-        return false;
-    }
+    return value === null || value === undefined || value === '';
+}
+
+export function requiredNumberIsNull(value: number | undefined | null) : boolean {
+    return value === null || value === undefined || value <= 0;
 }
 
 export function valueExceedsLength(value: string, length: number) : boolean {
-    if (value.length > length) {
-        return true;
-    } else {
-        return false;
+    return value.length > length;
+}
+
+export function userObject(user: any, token: string = '', refreshToken: string = '') : object {
+    return {
+        userId: user.user_id,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        email: user.email,
+        phoneNum: user.phone_number,
+        token: token,
+        refreshToken: refreshToken
+    }
+}
+
+export function tweetObject(tweet: Tweet) {
+    return {
+        tweetId: tweet.tweetId,
+        tweetText: tweet.tweetText,
+        createDate: tweet.createDateString,
+        userId: tweet.userId,
+        userDisplay: tweet.userDisplayName,
+        status: tweetValuesObject.createdOKMessage
     }
 }
