7e2b94ecd3f0d04b6c558fc81597624dbf722066
claypooltyler00 <claypooltyler00@gmail.com>
2024-02-04 22:28:14 -0600

    Added route to log in and sign JWT

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 const router = express.Router();
 import User from '../models/User';
-import { insertUser, phoneNumberExists, emailExists, usernameExists } from '../data_access/userService';
+import { insertUser, phoneNumberExists, emailExists, usernameExists, getUserByEmail } from '../data_access/userService';
 import { getError, getErrors, getStatus } from '../helpers/globalFunctions';
 import bcrypt from 'bcrypt';
 import { passwrdMeetsRequirements } from '../data_access/passwordService';
-import { passwordValuesObject, userValuesObject } from '../helpers/valuesHelper';
+import { passwordValuesObject, userValuesObject, errorsObject } from '../helpers/valuesHelper';
+import * as  emailValidator from 'email-validator';
+import { userObject } from '../helpers/modelHelper';
+import { generateToken } from '../helpers/jwtHelper';
 const saltRounds = 10;
+require('dotenv').config();
 
 router.post('/', async (req, resp) => {
     let user = new User();
@@ -74,6 +78,43 @@ router.post('/', async (req, resp) => {
         resp.status(500)
         .json(getStatus(ex));
     }
+});
+
+router.post('/login', async (req, resp) => {
+    let apiUser = new User();
+    apiUser.email = req.body.email;
+    apiUser.password = req.body.password;
+
+    apiUser.validateLoginInfo();
+
+    if (!emailValidator.validate(String(apiUser.email))) {
+        resp.status(400)
+        .json(getStatus(errorsObject.invalidEmailError));
+
+        return;
+    }
+
+    if (!emailExists(String(apiUser.email))) {
+        resp.status(404)
+        .json(getStatus(userValuesObject.emailExistsMessage))
+    }
+
+    const user = await getUserByEmail(String(apiUser.email));
+    apiUser.password = user.password;
+
+    apiUser.getUser(user);
+
+    if (!await bcrypt.compare(req.body.password, String(apiUser.password))) {
+        resp.status(400)
+        .json(getStatus(passwordValuesObject.incorrectPassswordMessage));
+    }
+
+    apiUser.password = '';
+
+    const token = generateToken(apiUser);
+
+    resp.status(200)
+    .send(getStatus(userObject(user, token)));
 });
 
 export default router;
